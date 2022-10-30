d0fd6b92aaf1631299553475a8372304be427479
elejeune11 <52667645+elejeune11@users.noreply.github.com>
2022-10-30 13:20:38 -0400

    initial functions and tests to read tiffs

--- src/microbundlecompute/image_analysis.py
+++ src/microbundlecompute/image_analysis.py
@@ -1,3 +1,40 @@
+import glob
+import numpy as np
+from pathlib import Path
+from skimage import io
+from typing import List
+
+
 def hello_microbundle_compute() -> str:
     "Given no input. Simple hello world as a test function."
     return "Hello World!"
+
+
+def read_tiff(img_path: Path) -> np.ndarray:
+    """Given a path to a tiff. Will return an array."""
+    img = io.imread(img_path)
+    return img
+
+
+def image_folder_to_path_list(folder_path: Path) -> List:
+    """Given a folder path. Will return the path to all files in that path in order."""
+    name_list = glob.glob(str(folder_path) + '/*.TIF')
+    name_list.sort()
+    name_list_path = []
+    for name in name_list:
+        name_list_path.append(Path(name))
+    return name_list_path
+
+
+def read_all_tiff(path_list: List) -> List:
+    """Given a folder path. Will return a list of all tiffs as an array."""
+    tiff_list = []
+    for path in path_list:
+        array = read_tiff(path)
+        tiff_list.append(array)
+    return tiff_list
+
+
+# segment shoshana images
+
+
--- tests/test_image_analysis.py
+++ tests/test_image_analysis.py
@@ -1,6 +1,49 @@
 from microbundlecompute import image_analysis as ia
+import numpy as np
+import os
+from pathlib import Path
+from skimage import io
 
 
 def test_hello_world():
     res = ia.hello_microbundle_compute()
     assert res == "Hello World!"
+
+
+def files_path():
+    self_path_file = Path(__file__)
+    self_path = self_path_file.resolve().parent
+    data_path = self_path.joinpath("files").resolve()
+    return data_path
+
+
+def example_path(example_name):
+    data_path = files_path()
+    example_path = data_path.joinpath(example_name).resolve()
+    return example_path
+
+
+def test_read_tiff():
+    data_path = files_path()
+    img_path = data_path.joinpath("segmentation_examples").resolve().joinpath("ex1_0000.TIF").resolve()
+    known = io.imread(img_path)
+    found = ia.read_tiff(img_path)
+    assert np.allclose(known, found)
+
+
+def test_image_folder_to_path_list():
+    folder_path = example_path("segmentation_examples")
+    name_list_path = ia.image_folder_to_path_list(folder_path)
+    assert len(name_list_path) == 5
+    for kk in range(0, 5):
+        assert os.path.isfile(name_list_path[kk])
+
+
+def test_read_all_tiff():
+    folder_path = example_path("segmentation_examples")
+    name_list_path = ia.image_folder_to_path_list(folder_path)
+    tiff_list = ia.read_all_tiff(name_list_path)
+    assert len(tiff_list) == 5
+    assert tiff_list[0].shape == (512, 512)
+
+
