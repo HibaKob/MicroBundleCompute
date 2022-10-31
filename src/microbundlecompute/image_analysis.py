ee2d5daa34205d46132df36fdf0280bf0756aa21
elejeune11 <52667645+elejeune11@users.noreply.github.com>
2022-10-30 21:27:47 -0400

    added functions

--- src/microbundlecompute/image_analysis.py
+++ src/microbundlecompute/image_analysis.py
@@ -1,8 +1,12 @@
 import glob
 import numpy as np
 from pathlib import Path
+from scipy import ndimage
 from skimage import io
-from typing import List
+from skimage.filters import threshold_otsu, gabor
+from skimage.measure import label, regionprops
+from skimage import morphology
+from typing import List, Union
 
 
 def hello_microbundle_compute() -> str:
@@ -35,6 +39,173 @@ def read_all_tiff(path_list: List) -> List:
     return tiff_list
 
 
-# segment shoshana images
+def apply_median_filter(array: np.ndarray, filter_size: int) -> np.ndarray:
+    """Given an image array. Will return the median filter applied by scipy"""
+    filtered_array = ndimage.median_filter(array, filter_size)
+    return filtered_array
 
 
+def apply_gaussian_filter(array: np.ndarray, filter_size: int) -> np.ndarray:
+    """Given an image array. Will return the gaussian filter applied by scipy"""
+    filtered_array = ndimage.gaussian_filter(array, filter_size)
+    return filtered_array
+
+
+def compute_otsu_thresh(array: np.ndarray) -> Union[float, int]:
+    """Given an image array. Will return the otsu threshold applied by skimage."""
+    thresh = threshold_otsu(array)
+    return thresh
+
+
+def apply_otsu_thresh(array: np.ndarray) -> np.ndarray:
+    """Given an image array. Will return a boolean numpy array with an otsu threshold applied."""
+    thresh = compute_otsu_thresh(array)
+    thresh_img = array > thresh
+    return thresh_img
+
+
+def get_region_props(array: np.ndarray) -> List:
+    """Given a binary image. Will return the list of region props."""
+    label_image = label(array)
+    region_props = regionprops(label_image)
+    return region_props
+
+
+def get_domain_center(array: np.ndarray) -> Union[int, float]:
+    """Given an array. Will return center (ix_0, ix_1)"""
+    center_0 = array.shape[0] / 2.0
+    center_1 = array.shape[1] / 2.0
+    return center_0, center_1
+
+
+def region_to_coords(regions_list: List) -> List:
+    """Given regions list. Will return the coordinates of all regions in the list."""
+    coords_list = []
+    for region in regions_list:
+        coords = region.coords
+        coords_list.append(coords)
+    return coords_list
+
+
+def coords_to_mask(coords_list: List, array: np.ndarray) -> np.ndarray:
+    """Given coordinates and template array. Will turn coordinates into a binary mask."""
+    mask = np.zeros(array.shape)
+    for coords in coords_list:
+        for kk in range(0, coords.shape[0]):
+            mask[coords[kk, 0], coords[kk, 1]] = 1
+    return mask
+
+
+def compute_distance(
+    a0: Union[int, float],
+    a1: Union[int, float],
+    b0: Union[int, float],
+    b1: Union[int, float]
+) -> Union[int, float]:
+    """Given two points. Will return distance between them."""
+    dist = ((a0 - b0)**2.0 + (a1 - b1)**2.0)**0.5
+    return dist
+
+
+def get_closest_region(
+    regions_list: List,
+    loc_0: Union[int, float],
+    loc_1: Union[int, float]
+) -> object:
+    """Given a list of region properties. Will return the object closest to location."""
+    center_dist = []
+    for region in regions_list:
+        centroid = region.centroid
+        region_0 = centroid[0]
+        region_1 = centroid[1]
+        dist = compute_distance(region_0, region_1, loc_0, loc_1)
+        center_dist.append(dist)
+    ix = np.argmin(center_dist)
+    return regions_list[ix]
+
+
+def get_largest_regions(region_props: List, num_regions: int = 3) -> List:
+    """Given a list of region properties. Will return a list of the num_regions largest regions.
+    If there are fewer than num_regions regions, will return all regions."""
+    area_list = []
+    for region in region_props:
+        area_list.append(region.area)
+    ranked = np.argsort(area_list)[::-1]
+    num_to_return = np.min([len(ranked), num_regions])
+    regions_list = []
+    for kk in range(0, num_to_return):
+        idx = ranked[kk]
+        regions_list.append(region_props[idx])
+    return regions_list
+
+
+def invert_mask(mask: np.ndarray) -> np.ndarray:
+    """Given a mask. Will return an inverted mask."""
+    invert_mask = mask == 0
+    return invert_mask
+
+
+def close_region(array: np.ndarray, radius: int = 1) -> np.ndarray:
+    """Given an array with a small hole. Will return a closed array."""
+    footprint = morphology.disk(radius, dtype=bool)
+    closed_array = morphology.binary_closing(array, footprint)
+    return closed_array
+
+
+def gabor_filter(array: np.ndarray, theta_range: int = 17, ff_max: int = 11, ff_mult: float = 0.1) -> np.ndarray:
+    gabor_all = np.zeros(array.shape)
+    for ff in range(0, ff_max):
+        frequency = 0.2 + ff * ff_mult
+        for tt in range(0, theta_range):
+            theta = tt * np.pi / (theta_range - 1)
+            filt_real, _ = gabor(array, frequency=frequency, theta=theta)
+            gabor_all += filt_real
+    return gabor_all
+
+
+def crop_boundaries(array: np.ndarray, crop: int = 20) -> np.ndarray:
+    array[0:crop, :] = 0
+    array[-crop:, :] = 0
+    array[:, 0:crop] = 0
+    array[:, -crop:] = 0
+    return array
+
+
+def microbundle_mask_v1(array: np.ndarray) -> np.ndarray:
+    """Given an image. Will segment the central microbundle and return it as a mask."""
+    median_filter_size = 5
+    array_median = apply_median_filter(array, median_filter_size)
+    gaussian_filter_size = 2
+    array_gaussian = apply_gaussian_filter(array_median, gaussian_filter_size)
+    thresh_img_inverted = apply_otsu_thresh(array_gaussian)
+    thresh_img_with_edges = invert_mask(thresh_img_inverted)
+    thresh_img = crop_boundaries(thresh_img_with_edges)
+    region_props = get_region_props(thresh_img)
+    center_0, center_1 = get_domain_center(array)
+    num_regions = 3
+    region_props_largest = get_largest_regions(region_props, num_regions)
+    microbundle_region = get_closest_region(region_props_largest, center_0, center_1)
+    mask = coords_to_mask([microbundle_region.coords], array)
+    radius = 3
+    closed_mask = close_region(mask, radius)
+    return closed_mask
+
+
+# def microbundle_mask_v2(array: np.ndarray) -> np.ndarray:
+#     """Given an image. Will segment the central microbundle and return it as a mask."""
+#     gabor_all = gabor_filter(array)
+#     median_filter_size = 5
+#     median_applied = apply_median_filter(gabor_all, median_filter_size)
+#     gaussian_filter_size = 2
+#     gaussian_applied = apply_gaussian_filter(median_applied, gaussian_filter_size)
+#     thresh_img_inverted = apply_otsu_thresh(gaussian_applied)
+#     thresh_img = invert_mask(thresh_img_inverted)
+#     region_props = get_region_props(thresh_img)
+#     center_0, center_1 = get_domain_center(array)
+#     num_regions = 3
+#     region_props_largest = get_largest_regions(region_props, num_regions)
+#     microbundle_region = get_closest_region(region_props_largest, center_0, center_1)
+#     mask = coords_to_mask([microbundle_region.coords], array)
+#     radius = 3
+#     closed_mask = close_region(mask, radius)
+#     return closed_mask
--- tests/test_image_analysis.py
+++ tests/test_image_analysis.py
@@ -1,8 +1,11 @@
+import matplotlib.pyplot as plt
 from microbundlecompute import image_analysis as ia
 import numpy as np
 import os
 from pathlib import Path
+from scipy import ndimage
 from skimage import io
+from skimage import morphology
 
 
 def test_hello_world():
@@ -45,5 +48,199 @@ def test_read_all_tiff():
     tiff_list = ia.read_all_tiff(name_list_path)
     assert len(tiff_list) == 5
     assert tiff_list[0].shape == (512, 512)
+
+
+def test_apply_median_filter():
+    array = np.ones((10, 10))
+    array[1, 5] = 10
+    array[7, 3] = 10
+    filter_size = 3
+    known = np.ones((10, 10))
+    found = ia.apply_median_filter(array, filter_size)
+    assert np.allclose(known, found)
+
+
+def test_apply_gaussian_filter():
+    array = np.ones((10, 10))
+    array[1, 5] = 10
+    array[7, 3] = 10
+    filter_size = 3
+    known = ndimage.gaussian_filter(array, filter_size)
+    found = ia.apply_gaussian_filter(array, filter_size)
+    assert np.allclose(known, found)
+
+
+def test_compute_otsu_thresh():
+    dim = 10
+    known_lower = 10
+    known_upper = 100
+    std_lower = 2
+    std_upper = 10
+    select = 0.8
+    x1 = np.random.normal(known_lower, std_lower, dim * dim * dim)
+    x1 = np.reshape(x1, (dim, dim, dim))
+    x2 = np.random.normal(known_upper, std_upper, dim * dim * dim)
+    x2 = np.reshape(x2, (dim, dim, dim))
+    choose = np.random.random((dim, dim, dim)) > select
+    x1[choose] = x1[choose] + x2[choose]
+    found = ia.compute_otsu_thresh(x1)
+    assert found > known_lower and found < (known_upper + known_lower)
+
+
+def test_apply_otsu_thresh():
+    dim = 10
+    known_lower = 10
+    known_upper = 10000
+    std_lower = 0.1
+    std_upper = 10
+    select = 0.8
+    x1 = np.random.normal(known_lower, std_lower, dim * dim * dim)
+    x1 = np.reshape(x1, (dim, dim, dim))
+    x2 = np.random.normal(known_upper, std_upper, dim * dim * dim)
+    x2 = np.reshape(x2, (dim, dim, dim))
+    choose = np.random.random((dim, dim, dim)) > select
+    x1[choose] = x1[choose] + x2[choose]
+    known = x1 > np.mean(x1)
+    found = ia.apply_otsu_thresh(x1)
+    assert np.allclose(known, found)
+
+
+def test_get_region_props():
+    rad_1 = 5
+    disk_1 = morphology.disk(rad_1, dtype=bool)
+    rad_2 = 3
+    disk_2 = morphology.disk(rad_2, dtype=bool)
+    dim = 30
+    array = np.zeros((dim, dim))
+    array[0:disk_1.shape[0], 0:disk_1.shape[1]] = disk_1
+    array[-disk_2.shape[0]:, -disk_2.shape[1]:] = disk_2
+    region_props = ia.get_region_props(array)
+    assert region_props[0].area == np.sum(disk_1)
+    assert region_props[1].area == np.sum(disk_2)
+
+
+def test_get_domain_center():
+    array = np.ones((10, 20))
+    center_0, center_1 = ia.get_domain_center(array)
+    assert center_0 == 5
+    assert center_1 == 10
+
+
+def test_region_to_coords():
+    rad_1 = 5
+    disk_1 = morphology.disk(rad_1, dtype=bool)
+    rad_2 = 3
+    disk_2 = morphology.disk(rad_2, dtype=bool)
+    rad_3 = 2
+    disk_3 = morphology.disk(rad_3, dtype=bool)
+    dim = 30
+    array = np.zeros((dim, dim))
+    array[0:disk_1.shape[0], 0:disk_1.shape[1]] = disk_1
+    array[-disk_2.shape[0]:, -disk_2.shape[1]:] = disk_2
+    array[0:disk_3.shape[0], -disk_3.shape[1]:] = disk_3
+    region_props = ia.get_region_props(array)
+    coords_list = ia.region_to_coords(region_props)
+    assert len(coords_list) == 3
+    assert coords_list[0].shape[1] == 2
+
+
+def test_coords_to_mask():
+    rad_1 = 5
+    disk_1 = morphology.disk(rad_1, dtype=bool)
+    region_props = ia.get_region_props(disk_1)
+    region = region_props[0]
+    coords = [region.coords]
+    mask = ia.coords_to_mask(coords, disk_1)
+    assert np.allclose(mask, disk_1)
+
+
+def test_compute_distance():
+    a0 = 0
+    a1 = 0
+    b0 = 10
+    b1 = 0
+    known = 10
+    found = ia.compute_distance(a0, a1, b0, b1)
+    assert known == found
+
+
+def test_get_largest_regions():
+    rad_1 = 5
+    disk_1 = morphology.disk(rad_1, dtype=bool)
+    rad_2 = 3
+    disk_2 = morphology.disk(rad_2, dtype=bool)
+    rad_3 = 2
+    disk_3 = morphology.disk(rad_3, dtype=bool)
+    dim = 30
+    array = np.zeros((dim, dim))
+    array[0:disk_1.shape[0], 0:disk_1.shape[1]] = disk_1
+    array[-disk_2.shape[0]:, -disk_2.shape[1]:] = disk_2
+    array[0:disk_3.shape[0], -disk_3.shape[1]:] = disk_3
+    region_props = ia.get_region_props(array)
+    num_regions = 2
+    regions_list = ia.get_largest_regions(region_props, num_regions)
+    assert len(regions_list) == 2
+    assert regions_list[0].area == np.sum(disk_1)
+    assert regions_list[1].area == np.sum(disk_2)
+
+
+def test_invert_mask():
+    array_half = np.zeros((10, 10))
+    array_half[0:5, :] = 1
+    array_invert = ia.invert_mask(array_half)
+    assert np.allclose(array_invert + array_half, np.ones((10, 10)))
+
+
+def test_close_region():
+    val = 10
+    array = np.zeros((val, val))
+    array[3:7, 3:7] = 1
+    array_missing = np.copy(array)
+    array_missing[5, 5] = 0
+    array_closed = ia.close_region(array_missing)
+    assert np.allclose(array_closed, array)
+
+
+def test_gabor_filter():
+    array = np.zeros((10, 10))
+    gabor_all = ia.gabor_filter(array)
+    assert np.sum(gabor_all) == 0
+    data_path = files_path()
+    img_path = data_path.joinpath("segmentation_examples").resolve().joinpath("ex1_0000.TIF").resolve()
+    array = io.imread(img_path)
+    gabor_all = ia.gabor_filter(array)
+    assert np.allclose(gabor_all, array) is False
+
+
+def test_crop_boundaries():
+    array = np.ones((100, 100))
+    cropped_array = ia.crop_boundaries(array)
+    assert np.sum(cropped_array) == (100 - 20 * 2) ** 2.0
+
+
+def test_microbundle_mask_v1():
+    data_path = files_path()
+    img_path = data_path.joinpath("segmentation_examples").resolve().joinpath("ex1_0000.TIF").resolve()
+    array = io.imread(img_path)
+    mask = ia.microbundle_mask_v1(array)
+    assert np.max(mask) == 1
+    assert np.min(mask) == 0
+    assert mask.shape[0] == array.shape[0]
+    assert mask.shape[1] == array.shape[1]
+
+
+def test_microbundle_mask_all_v1():
+    folder_path = example_path("segmentation_examples")
+    path_save = files_path()
+    name_list_path = ia.image_folder_to_path_list(folder_path)
+    tiff_list = ia.read_all_tiff(name_list_path)
+    mask_list = []
+    for kk in range(0, len(tiff_list)):
+        mask = ia.microbundle_mask_v1(tiff_list[kk])
+        mask_list.append(mask)
+        plt.figure()
+        plt.imshow(mask)
+        plt.savefig(str(path_save) + "%i_v1.png" % (kk))
+    assert len(mask_list) == 5
 
 
