bc910152b0c6b071eb2383c57eb75bb7e2e99c57
HibaKob <hibakob@bu.edu>
2023-04-19 04:18:40 -0400

    modified:   tests/__pycache__/test_image_analysis.cpython-39-pytest-7.1.2.pyc 	modified:   tests/__pycache__/test_strain_analysis.cpython-39-pytest-7.1.2.pyc 	new file:   tests/files/real_example_short_small_angle_rotated/results/strain__beat0_Fcc.txt 	new file:   tests/files/real_example_short_small_angle_rotated/results/strain__beat0_Fcr.txt 	new file:   tests/files/real_example_short_small_angle_rotated/results/strain__beat0_Frc.txt 	new file:   tests/files/real_example_short_small_angle_rotated/results/strain__beat0_Frr.txt 	new file:   tests/files/real_example_short_small_angle_rotated/results/strain__beat0_col.txt 	new file:   tests/files/real_example_short_small_angle_rotated/results/strain__beat0_row.txt 	new file:   tests/files/real_example_short_small_angle_rotated/results/strain__sub_domain_info.txt 	new file:   tests/files/real_non_square_example_short_rotated/results/strain__beat0_Fcc.txt 	new file:   tests/files/real_non_square_example_short_rotated/results/strain__beat0_Fcr.txt 	new file:   tests/files/real_non_square_example_short_rotated/results/strain__beat0_Frc.txt 	new file:   tests/files/real_non_square_example_short_rotated/results/strain__beat0_Frr.txt 	new file:   tests/files/real_non_square_example_short_rotated/results/strain__beat0_col.txt 	new file:   tests/files/real_non_square_example_short_rotated/results/strain__beat0_row.txt 	new file:   tests/files/real_non_square_example_short_rotated/results/strain__sub_domain_info.txt 	modified:   tests/test_strain_analysis.py

--- tests/test_strain_analysis.py
+++ tests/test_strain_analysis.py
@@ -370,6 +370,31 @@ def test_run_sub_domain_strain_analysis():
         assert sap.is_file()
         
 
+def test_run_sub_domain_strain_analysis_non_square():
+    input_path = example_path("real_non_square_example_short_rotated")
+    fps = 1
+    length_scale = 1
+    _ = ia.run_tracking(input_path, fps, length_scale)
+    saved_paths = sa.run_sub_domain_strain_analysis(input_path)
+    for sap in saved_paths:
+        assert sap.is_file()
+    saved_paths = sa.run_sub_domain_strain_analysis(input_path, is_rotated=False)
+    for sap in saved_paths:
+        assert sap.is_file()
+
+
+def test_run_sub_domain_strain_analysis_small_angle():
+    input_path = example_path("real_example_short_small_angle_rotated")
+    fps = 1
+    length_scale = 1
+    _ = ia.run_tracking(input_path, fps, length_scale)
+    saved_paths = sa.run_sub_domain_strain_analysis(input_path)
+    for sap in saved_paths:
+        assert sap.is_file()
+    saved_paths = sa.run_sub_domain_strain_analysis(input_path, is_rotated=False)
+    for sap in saved_paths:
+        assert sap.is_file()
+
 
 def test_get_text_str():
     known_str = "A1"
